21d3deb3cc4b96bfee61f550921558ebc23a4bdc
idooru <shere1765@gmail.com>
2024-08-24 16:12:48 +0900

    주문과 할인 도메인 개발

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.*;
+
+public class OrderServiceImpl implements OrderService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        final Member findMember = memberRepository.findById(memberId);
+        final int discountPrice = discountPolicy.discount(findMember, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
